cc9c37deac02a51a1e1682351d2456253f7f9fb1
Markus Bilz <mail@markusbilz.com>
2017-03-26 20:49:28 +0200

    Clean up for Spotify API

--- SSOS/application.py
+++ SSOS/application.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 import base64
 import os
-
 import nltk
 import requests
-import six
+
 from selenium import webdriver
+
 
 class Artist(object):
     def __init__(self):
         self.artist_name = None
         self.artist_uri = None
         self.albums = None
+        # can contain array with dicts containing track_uri, track_name and valence
         self.tracks = None
 
-class Spotify(object):
 
-    def _get(self, url, payload=None, **kwargs):
+class Spotify(object):
+    def _get(self, url, payload=None):
+        """
+        Generalized get function to access endpoints
+        :param url: url specifying endpoint e. g. https://api.spotify.com/v1/audio-features/{id}
+        :param payload: parameters e.g. {q:'albums'}
+        :return: json: response as json.
+        """
         headers = {'Accept': 'application/json'}
         r = requests.get(self.api_endpoint.format(url), params=payload, headers=headers)
         if r.status_code == requests.codes.ok:
@@ -24,85 +31,89 @@ class Spotify(object):
         else:
             return None
 
-
-    def spotify_get_artist(self, artist_name):
-        params = {'q': artist_name, 'type': 'artist'}
-        # search response for artist name and uri
-        json = self._get('search', params)['artists']["items"]
-        seed_artists = [{'artist_name': i['name'], 'artist_uri': str.replace(i['uri'], 'spotify:artist:', '')} for i in
-                        json]
-
-        # return first match from seed_artists, assume its best.
-        return [seed_artist for seed_artist in seed_artists if seed_artist['artist_name'] == artist_name][0]
-
-
-    def spotify_get_album_id(self, artist):
-
-        params = {'album_type': 'album'}
-        album_url = "artists/{}/albums".format(artist['artist_uri'])
-
-        # extract relevant information
-        json = self._get(album_url, params)["items"]
-        seed_albums = [{'album_name': i['name'], 'album_uri': str.replace(i['uri'], 'spotify:album:', '')}
-                       for i in json]
-
-        # save albums name and uri to different lists
-        # artist['album_name'] = [albums['album_name'] for albums in seed_albums]
-        # artist['album_uri'] = [albums['album_uri'] for albums in seed_albums]
-        artist['albums'] = seed_albums
-        return artist
-
-
     def _authenticate(self):
-        # todo: handle refreshing of token better...
-        auth_header = base64.b64encode(six.text_type(self.spotify_client_id + ':' + self.spotify_client_secret).encode('ascii'))
-        headers = {'Authorization': 'Basic %s' % auth_header.decode('ascii')}
+        """
+        authenticate function to obtain access token. spotify_client_id and spotifiy_client_secret
+        must be set. Token can expire. Only some API queries require token.
+        :return:
+        """
+        auth_header = base64.b64encode((self.spotify_client_id + ':' + self.spotify_client_secret).encode('ascii'))
+        headers = {'Authorization': 'Basic {}'.format(auth_header.decode('ascii'))}
         payload = {'grant_type': 'client_credentials'}
         r = requests.post('https://accounts.spotify.com/api/token', data=payload, headers=headers, verify=True)
         if r.status_code is not 200:
             raise RuntimeError(r.reason)
         return r.json()['access_token']
 
-
-    def spotify_get_album_tracks(self, artist):
-        """ max. 50 tracks per Album ....
+    def spotify_get_artist(self, artist_name):
         """
+        Search for artist_uri by artist name.
+        :param artist_name:
+        :return: artist: containing artist_uri, and artist_name
+        """
+        params = {'q': artist_name, 'type': 'artist'}
+        # search response for artist name and uri
+        json = self._get('search', params)['artists']["items"]
+        artists = [{'artist_name': i['name'], 'artist_uri': str.replace(i['uri'], 'spotify:artist:', '')}
+                        for i in json]
 
+        # return 1:1 match in seed_artists list, strings are lowercased
+        return [artist for artist in artists
+                if str.lower(artist['artist_name']) == str.lower(artist_name)][0]
+
+    def spotify_get_albums(self, artist_uri):
+        """
+        Search for all albums, not singles etc. for given artist
+        :param artist_uri:
+        :return: albums: array of albums for given artist
+        """
+        params = {'album_type': 'album'}
+        album_url = "artists/{}/albums".format(artist_uri)
+
+        # extract relevant information
+        json = self._get(album_url, params)["items"]
+        albums = [{'album_name': i['name'], 'album_uri': str.replace(i['uri'], 'spotify:album:', '')}
+                  for i in json]
+        return albums
+
+    def spotify_get_album_tracks(self, albums):
+        """
+        Query tracks for given album array. At maximum 50 tracks can be searched,
+         due to restrictions of the spotify API.
+        :param albums:
+        :return: tracks: array with tracks for ALL albums
+        """
         access_token = self._authenticate()
-        album_uri = artist['album_uri']
-        seed_tracks = []
-
+        album_uri = [album['album_uri'] for album in albums]
+        tracks = []
         # search for track_uri
         for i in album_uri:
             params = {'access_token': access_token, 'limit': 50}
-            headers = {'Accept': 'application/json'}
-            url = "https://api.spotify.com/v1/albums/{}/tracks".format(i)
-            r = requests.get(url, params=params, headers=headers)
-            json = r.json()['items']
-            tracks_per_album = [{'track_name': i['name'], 'track_uri': str.replace(i['uri'],
-                                                                                   'spotify:track:', '')} for i in json]
-            seed_tracks += tracks_per_album
+            url = "albums/{}/tracks".format(i)
+            json = self._get(url, params)['items']
+            tracks_per_album = [{'track_name': i['name'], 'track_uri': str.replace(i['uri'],'spotify:track:', '')}
+                                for i in json]
+            tracks += tracks_per_album
+        return tracks
 
-        # save to two arrays
-        artist['track_name'] = [tracks['track_name'] for tracks in seed_tracks]
-        artist['track_uri'] = [tracks['track_uri'] for tracks in seed_tracks]
-        return artist
-
-
-    def spotify_get_audio_features(self, artist):
+    def spotify_get_audio_features(self, tracks):
+        """
+        Get valence score for tracks list, append it to each dictionary.
+        :param tracks: tracks array containing track's name and uri.
+        :return: tracks: tracks including valence score.
+        """
         access_token = self._authenticate()
-        track_uri = artist['track_uri']
-        seed_audio_features = []
+        track_uri = [track['track_uri'] for track in tracks]
+        audio_features = []
         for i in track_uri:
             params = {'access_token': access_token}
-            headers = {'Accept': 'application/json'}
-            url = "https://api.spotify.com/v1/audio-features/{}".format(i)
-            r = requests.get(url, params=params, headers=headers)
-            json = r.json()
-            seed_audio_features += [json['valence']]
-        artist['track_valence'] = seed_audio_features
-        return artist
+            url = "audio-features/{}".format(i)
+            json = self._get(url, params)['valence']
+            audio_features += [json]
 
+        for index, item in enumerate(tracks):
+            tracks[index]['valence'] = audio_features[index]
+        return tracks
 
     def genius_get_artist(self, artist_name, access_token):
         """
@@ -134,7 +145,6 @@ class Spotify(object):
             page += 1
         return artist_id
 
-
     def genius_get_lyrics(self, artist_id, access_token):
         """
         Function returns a list of songs for a given artist.
@@ -143,10 +153,7 @@ class Spotify(object):
         :return: list with downloaded data. Contains url, title etc.
         """
         url = "https://api.genius.com/artists/{}/songs".format(artist_id)
-        # user-agent is required otherwise 403
-        headers = {'Authorization': 'Bearer ' + access_token,
-                   'user-agent': 'curl/7.9.8 (i686-pc-linux-gnu) \
-                            libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)'}
+        headers = {'Authorization': 'Bearer ' + access_token}
 
         # array for results, start with page 1
         track_lyric_urls = []
@@ -167,7 +174,6 @@ class Spotify(object):
                 break
         return track_lyric_urls
 
-
     def lyric_scraper(self, url):
         """
         function crawls web page by given url. PhantomJS or any other web browser
@@ -181,7 +187,6 @@ class Spotify(object):
         driver.get(url)
         lyrics = driver.find_element_by_class_name('lyrics').text
         return lyrics
-
 
     def lyric_analysis(self, lyrics):
         """
@@ -222,7 +227,6 @@ class Spotify(object):
         except KeyError:
             raise RuntimeError("client_data not set")
 
-
     def main(self):
         """
         search_term = 'Adele'
@@ -239,21 +243,17 @@ class Spotify(object):
         spotify_artist = self.spotify_get_artist("The Rasmus")
         artist.artist_name = spotify_artist['artist_name']
         artist.artist_uri = spotify_artist['artist_uri']
-
-
+        artist.albums = self.spotify_get_albums(artist.artist_uri)
+        artist.tracks = self.spotify_get_album_tracks(artist.albums)
+        artist.tracks = self.spotify_get_audio_features(artist.tracks)
         print(vars(artist))
 
-        # albums = self.spotify_get_album_id(artist)
-        # print(albums)
-        """
-        tracks = spotify_get_album_tracks(albums)
-        saddest_song = spotify_get_audio_features(tracks)
-        """
         """
         # Test for Eminem, loose yourself
         lyrics = "The clock's run out, time's up, over, bloah!"
         print(lyric_analysis(lyrics))
         """
+
 
 custom_spotify = Spotify()
 custom_spotify.main()
